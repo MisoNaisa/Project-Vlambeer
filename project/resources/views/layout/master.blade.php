a9cc6854ea6e083cd967baaeb4eb0cb41e3403cd
Sjoerd <sjoerd_3@live.nl>
2015-11-19 12:08:38 +0100

    added master blade

---
+++ project/resources/views/layout/master.blade.php
@@ -0,0 +1,58 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <title>Vlambeer</title>
+</head>
+<body>
+
+
+
+<!-- INSERT HTML -->
+
+
+<div class="container">
+    <div class="footer">
+
+        <div class="footer-left col-xs-8">
+            <p>VLAMBEER IS A DUTCH INDEPENDENT GAME STUDIO made up of
+            Rami Ismail and Jan Willem Nijman,
+            bringing back arcade games since 1864.</p>
+        </div>
+
+        <div class="footer-right col-xs-4">
+            <h3>Contact</h3>
+            <div class="contact-rami">
+                <p>Rami Ismail</p>
+                <p>Business and Development</p>
+                <a href="">rami@vlambeer.com</a>
+                <a href="">@tha_rami</a>
+            </div>
+            <div class="contact-jan">
+                <p>Jan Willem Nijman</p>
+                <p>Game Design</p>
+                <a href="">jw@vlambeer.com</a>
+                <a href="">@jwaaaap</a>
+            </div>
+            <div class="contact-vlambeer">
+                <p>Vlambeer</p>
+                <a href="">@vlambeer</a>
+            </div>
+
+            <div class="contact-friends">
+                <a href="">Friends</a>
+            </div>
+
+            <div class="contact-info">
+                <p><a href="">info@vlambeer.com</a> | Neude 5, 3512 AD, Utrecht, the Netherlands | +31621206363</p>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+</body>
+</html>
