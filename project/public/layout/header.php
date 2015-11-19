bdd82f827e19ce3f5ebfffed00c6cae41dc6049b
jordeytje <jordeytje@live.nl>
2015-11-19 11:56:23 +0100

    header toegevoegd
    spreekt voor zichzelf

---
+++ project/public/layout/header.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jordey
+ * Date: 19-11-2015
+ * Time: 11:47
+ */
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <title>Vlambeer</title>
+</head>
+<body>
+
+</body>
+</html>
