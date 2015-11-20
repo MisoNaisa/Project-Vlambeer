07c38fa5dfe415b9dced0ae87b3785e138cc1fcf
jordeytje <jordeytje@live.nl>
2015-11-20 13:17:03 +0100

    index.blade html & css gefixt
    het werkt eindelijk jonguh

--- project/resources/views/pages/index.blade.php
+++ project/resources/views/pages/index.blade.php
@@ -1,1 +1,26 @@
 @extends('layout.master')
+
+@section('section')
+    <div class="game-post">
+        <div class="title">
+            <h1>Kazenspel</h1>
+            <h3>Word een met je innerlijke beer</h3>
+        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam maxime nobis placeat, quam quasi quos!
+            A ad aut blanditiis dicta eius eos error et minima nihil, nobis pariatur quam voluptatum.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque eveniet iste nihil recusandae veritatis? A ad cum, dolorum eaque,
+            excepturi nobis odio officiis quas reiciendis reprehenderit sequi, temporibus veniam voluptate?</p>
+        <div class="stripe-left"></div>
+    </div>
+    <div class="game-post">
+        <div class="title">
+            <h1>Kazenspel</h1>
+            <h3>Word een met je innerlijke beer</h3>
+        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam maxime nobis placeat, quam quasi quos!
+            A ad aut blanditiis dicta eius eos error et minima nihil, nobis pariatur quam voluptatum.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque eveniet iste nihil recusandae veritatis? A ad cum, dolorum eaque,
+            excepturi nobis odio officiis quas reiciendis reprehenderit sequi, temporibus veniam voluptate?</p>
+        <div class="stripe-right"></div>
+    </div>
+@endsection
