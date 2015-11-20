e4e6e51636dc03f396dec914999296fda8389612
kimvg2 <kimvg2@hotmail.com>
2015-11-20 11:50:19 +0100

    oke heeeeeeel veel meuk. en de footer alvast begonnen met css
    ff committe tussedoor om bitchy github te voorkomen

--- project/resources/views/layout/master.blade.php
+++ project/resources/views/layout/master.blade.php
@@ -3,8 +3,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="/vendor/main.css">
-    <link rel="stylesheet" href="/vendor/bootstrap.css">
+    <link rel="stylesheet" href="vendors/main.css">
+    <link rel="stylesheet" href="vendors/bootstrap.css">
     <title>Vlambeer</title>
 </head>
 <body>
@@ -36,42 +36,42 @@
 
 <!-- Begin Footer -->
     <div class="footer">
+        <div class="row">
+            <div class="footer-left col-xs-7">
+                <p>VLAMBEER IS A DUTCH INDEPENDENT GAME STUDIO made up of
+                Rami Ismail and Jan Willem Nijman,
+                bringing back arcade games since 1864.</p>
+            </div>
 
-        <div class="footer-left col-xs-8">
-            <p>VLAMBEER IS A DUTCH INDEPENDENT GAME STUDIO made up of
-            Rami Ismail and Jan Willem Nijman,
-            bringing back arcade games since 1864.</p>
+            <div class="footer-right col-xs-5">
+                <h3>Contact</h3>
+                <div class="contact-rami">
+                    <p>Rami Ismail</p>
+                    <p>Business and Development</p>
+                    <a href="">rami@vlambeer.com</a>
+                    <a href="">@tha_rami</a>
+                </div>
+                <div class="contact-jan">
+                    <p>Jan Willem Nijman</p>
+                    <p>Game Design</p>
+                    <a href="">jw@vlambeer.com</a>
+                    <a href="">@jwaaaap</a>
+                </div>
+                <div class="contact-vlambeer">
+                    <p>Vlambeer</p>
+                    <a href="">@vlambeer</a>
+                </div>
+
+                <div class="contact-friends">
+                    <a href="">Friends</a>
+                </div>
+
+                <div class="contact-info">
+                    <p><a href="">info@vlambeer.com</a> | Neude 5, 3512 AD, Utrecht, the Netherlands | +31621206363</p>
+                </div>
+
+            </div>
         </div>
-
-        <div class="footer-right col-xs-4">
-            <h3>Contact</h3>
-            <div class="contact-rami">
-                <p>Rami Ismail</p>
-                <p>Business and Development</p>
-                <a href="">rami@vlambeer.com</a>
-                <a href="">@tha_rami</a>
-            </div>
-            <div class="contact-jan">
-                <p>Jan Willem Nijman</p>
-                <p>Game Design</p>
-                <a href="">jw@vlambeer.com</a>
-                <a href="">@jwaaaap</a>
-            </div>
-            <div class="contact-vlambeer">
-                <p>Vlambeer</p>
-                <a href="">@vlambeer</a>
-            </div>
-
-            <div class="contact-friends">
-                <a href="">Friends</a>
-            </div>
-
-            <div class="contact-info">
-                <p><a href="">info@vlambeer.com</a> | Neude 5, 3512 AD, Utrecht, the Netherlands | +31621206363</p>
-            </div>
-
-        </div>
-
     </div>
     <!-- End Footer-->
 </div>
