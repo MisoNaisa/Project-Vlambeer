06111011bf624f217158c33bdc6247eccbc29058
jordeytje <jordeytje@live.nl>
2015-11-23 09:06:11 +0100

    index vernieuwd
    heel fancy enzo

--- project/resources/views/pages/index.blade.php
+++ project/resources/views/pages/index.blade.php
@@ -12,7 +12,22 @@
             excepturi nobis odio officiis quas reiciendis reprehenderit sequi, temporibus veniam voluptate?</p>
         <div id="steam"></div>
     </div>
-    <div class="stripe-under-left"></div>
+
+    <div class="game-post">
+        <div class="shaped-above">
+            <div class="title">
+                <h1>Kazenspel</h1>
+                <h3>Word een met je innerlijke beer</h3>
+            </div>
+        </div>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam maxime nobis placeat, quam quasi quos!
+            A ad aut blanditiis dicta eius eos error et minima nihil, nobis pariatur quam voluptatum.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque eveniet iste nihil recusandae veritatis? A ad cum, dolorum eaque,
+            excepturi nobis odio officiis quas reiciendis reprehenderit sequi, temporibus veniam voluptate?</p>
+        <div class="shaped-under"></div>
+    </div>
+
     <div class="game-post">
         <div class="title">
             <h1>Kazenspel</h1>
@@ -22,6 +37,36 @@
             A ad aut blanditiis dicta eius eos error et minima nihil, nobis pariatur quam voluptatum.
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque eveniet iste nihil recusandae veritatis? A ad cum, dolorum eaque,
             excepturi nobis odio officiis quas reiciendis reprehenderit sequi, temporibus veniam voluptate?</p>
+        <div id="steam"></div>
     </div>
-    <div class="stripe-upper-left"></div>
+
+    <div class="game-post">
+        <div class="shaped-above">
+            <div class="title">
+                <h1>Kazenspel</h1>
+                <h3>Word een met je innerlijke beer</h3>
+            </div>
+        </div>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam maxime nobis placeat, quam quasi quos!
+            A ad aut blanditiis dicta eius eos error et minima nihil, nobis pariatur quam voluptatum.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque eveniet iste nihil recusandae veritatis? A ad cum, dolorum eaque,
+            excepturi nobis odio officiis quas reiciendis reprehenderit sequi, temporibus veniam voluptate?</p>
+        <div class="shaped-under"></div>
+    </div>
+
+    <div class="game-post">
+        <div class="title">
+            <h1>Kazenspel</h1>
+            <h3>Word een met je innerlijke beer</h3>
+        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam maxime nobis placeat, quam quasi quos!
+            A ad aut blanditiis dicta eius eos error et minima nihil, nobis pariatur quam voluptatum.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque eveniet iste nihil recusandae veritatis? A ad cum, dolorum eaque,
+            excepturi nobis odio officiis quas reiciendis reprehenderit sequi, temporibus veniam voluptate?</p>
+        <div id="steam"></div>
+    </div>
+
+
+
 @endsection
