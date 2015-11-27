5ed4889103688cfed39138929b659a2d4aa5c122
Alex <alex.kuo@ictmbo.nl>
2015-11-27 11:23:13 +0100

    contact page maps + styling

--- project/resources/views/pages/contact.blade.php
+++ project/resources/views/pages/contact.blade.php
@@ -22,7 +22,7 @@
                     rami@vlambeer.com <br>
                     @tha_rami <br>
                 </p>
-            </div>`
+            </div>
             <div class="col-xs-2">
                 <img src="img/team_jw.png" alt="team_jw">
             </div>
@@ -35,14 +35,13 @@
                 </p>
             </div>
         </div>
-    </div>
-
-    <div class="companyLocation col-xs-12">
-        <p>
-            Facebook<br><br>
-            info@vlambeer.com | Neude 5, 3512 AD<br>
-            Utrecht, The Netherlands | +316 21 20 63 63
-        </p>
-        <iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJvwsb9kRvxkcR1a9sC9TEo9k&key=AIzaSyCcK7dBjnowNw9k4E0pfeLCN-7wcEil66E" allowfullscreen></iframe>
+        <div class="companyLocation col-xs-12">
+            <p>
+                Facebook<br><br>
+                info@vlambeer.com | Neude 5, 3512 AD<br>
+                Utrecht, The Netherlands | +316 21 20 63 63
+            </p>
+            <iframe class="maps" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJvwsb9kRvxkcR1a9sC9TEo9k&key=AIzaSyCcK7dBjnowNw9k4E0pfeLCN-7wcEil66E" allowfullscreen></iframe>
+        </div>
     </div>
 @endsection
