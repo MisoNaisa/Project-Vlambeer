f41de64307745d71a2f23953179e932829b6bedb
Alex <alex.kuo@ictmbo.nl>
2015-11-27 12:29:41 +0100

    fixed link syling

--- project/resources/views/pages/contact.blade.php
+++ project/resources/views/pages/contact.blade.php
@@ -19,9 +19,9 @@
                 <p>
                     Rami Ismail<br>
                     Business and Development <br>
-                    <a href="mailto:rami@vlambeer.com">rami@vlambeer.com</a>
+                    <a href="mailto:rami@vlambeer.com" class="link">rami@vlambeer.com</a>
                      <br>
-                    <a href="https://twitter.com/search?q=tha_rami&src=typd">@tha_rami <i class="fa fa-twitter"></i></a> <br>
+                    <a href="https://twitter.com/search?q=tha_rami&src=typd" class="link">@tha_rami <i class="fa fa-twitter"></i></a> <br>
                 </p>
             </div>
             <div class="col-xs-2 jan picture">
@@ -31,16 +31,16 @@
                 <p>
                     Jan Willem Nijman<br>
                     Game Design <br>
-                    <a href="mailto:jw@vlambeer.com">jw@vlambeer.com</a>
+                    <a href="mailto:jw@vlambeer.com" class="link">jw@vlambeer.com</a>
                      <br>
-                    <a href="https://twitter.com/jwaaaap">@jwaaaap <i class="fa fa-twitter"></i></a> <br>
+                    <a href="https://twitter.com/jwaaaap" class="link">@jwaaaap <i class="fa fa-twitter"></i></a> <br>
                 </p>
             </div>
         </div>
         <div class="companyLocation col-xs-12">
             <p>
-                <a href="https://www.facebook.com/Vlambeer/?fref=ts">Like us on Facebook! <i class="fa fa-facebook-square"></i></a><br><br>
-                <a href="mailto:info@vlambeer.com">info@vlambeer.com</a> | Neude 5, 3512 AD<br>
+                <a href="https://www.facebook.com/Vlambeer/?fref=ts" class="link">Like us on Facebook! <i class="fa fa-facebook-square"></i></a><br><br>
+                <a href="mailto:info@vlambeer.com" class="link">info@vlambeer.com</a> | Neude 5, 3512 AD<br>
                 Utrecht, The Netherlands | +316 21 20 63 63
             </p>
             <iframe class="maps" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJvwsb9kRvxkcR1a9sC9TEo9k&key=AIzaSyCcK7dBjnowNw9k4E0pfeLCN-7wcEil66E" allowfullscreen></iframe>
