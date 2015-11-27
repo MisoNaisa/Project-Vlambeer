189974a85c8aae8a9ba0dcf5a4fe7eceae8bc1c9
Alex <alex.kuo@ictmbo.nl>
2015-11-27 09:15:56 +0100

    Contact page + overview games page

--- project/resources/views/pages/contact.blade.php
+++ project/resources/views/pages/contact.blade.php
@@ -1,1 +1,54 @@
 @extends('layout.master')
+
+@section('section')
+    <div class="container">
+        <div class="col-xs-6">
+            <h3>Contact</h3>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Adipisci aliquid autem dignissimos eum ex explicabo illo ipsum laboriosam laborum,
+                maxime minus molestias natus odio porro quibusdam tenetur voluptas voluptate voluptatibus.
+            </p>
+        </div>
+
+        <div class="col-xs-6">
+            <div class="col-xs-2">
+                <img src="img/team_rami.png" alt="team_rami">
+            </div>
+            <div class="col-xs-10">
+                <p>
+                    Rami Ismail<br>
+                    Business and Development <br>
+                    rami@vlambeer.com <br>
+                    @tha_rami <br>
+                </p>
+            </div>`
+            <div class="col-xs-2">
+                <img src="img/team_jw.png" alt="team_jw">
+            </div>
+            <div class="col-xs-10">
+                <p>
+                    Rami Ismail<br>
+                    Business and Development <br>
+                    rami@vlambeer.com <br>
+                    @tha_rami <br>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="companyLocation">
+        <p>
+            Facebook<br><br>
+            info@vlambeer.com | Neude 5, 3512 AD<br>
+            Utrecht, The Netherlands | +316 21 20 63 63
+        </p>
+        <iframe
+                width="600"
+                height="450"
+                frameborder="0" style="border:0"
+                src="https://www.google.com/maps/embed/v1/place?key=YOUR_API_KEY
+    &q=Space+Needle,Seattle+WA" allowfullscreen>
+        </iframe>sssssjjjjjjzzijn
+    </div>
+@endsection
--- project/resources/views/pages/overview_games.blade.php
+++ project/resources/views/pages/overview_games.blade.php
@@ -1,31 +1,31 @@
 @extends('layout.master')
-<div class="section">
+<div class="container">
     <div class="row">
-        <div class="col-md-5 game-box">
+        <div class="col-md-5 col-md-push-1 game-box">
             <img src="http://lorempixel.com/300/300" alt="game-image">
             <h1>Kazenspel</h1>
         </div>
-        <div class="col-md-5 game-box">
+        <div class="col-md-5 col-md-push-1 game-box">
             <img src="http://lorempixel.com/300/300" alt="game-image">
             <h1>Berenspel</h1>
         </div>
     </div>
     <div class="row">
-        <div class="col-md-5 game-box">
+        <div class="col-md-5 col-md-push-1 game-box">
             <img src="http://lorempixel.com/300/300" alt="game-image">
             <h1>Kazenspel</h1>
         </div>
-        <div class="col-md-5 game-box">
+        <div class="col-md-5 col-md-push-1 game-box">
             <img src="http://lorempixel.com/300/300" alt="game-image">
             <h1>Berenspel</h1>
         </div>
     </div>
     <div class="row">
-        <div class="col-md-5 game-box">
+        <div class="col-md-5 col-md-push-1 game-box">
             <img src="http://lorempixel.com/300/300" alt="game-image">
             <h1>Kazenspel</h1>
         </div>
-        <div class="col-md-5 game-box">
+        <div class="col-md-5 col-md-push-1 game-box">
             <img src="http://lorempixel.com/300/300" alt="game-image">
             <h1>Berenspel</h1>
         </div>
