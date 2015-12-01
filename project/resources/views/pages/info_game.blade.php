0d078000cecb45db1ddb30a6f0bee0d38d02ebed
jordeytje <jordeytje@live.nl>
2015-12-01 22:04:59 +0100

    video update

--- project/resources/views/pages/info_game.blade.php
+++ project/resources/views/pages/info_game.blade.php
@@ -2,16 +2,14 @@
 
 @section('section')
 
-
-
-
-    <div style="width: 100%; height: 500px;"
+    {{--style="width: 100%; height: 500px;--}}
+    <div
          data-vide-bg="webm: ../video/serious-sam"
          data-vide-options="posterType: jpg, loop: true, muted: true, position: 0% 0%">
     </div>
 
     <div class="container">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto asperiores at commodi cum, distinctio error
+        <p>1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto asperiores at commodi cum, distinctio error
             facere facilis impedit ipsam modi molestiae natus nulla, officiis quaerat rem sit unde voluptate.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto asperiores at commodi cum, distinctio error
             facere facilis impedit ipsam modi molestiae natus nulla, officiis quaerat rem sit unde voluptate.</p>
