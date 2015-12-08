cf11cf7e730ade66ce37d4aaae830d2864331928
kimvg2 <kimvg2@hotmail.com>
2015-12-08 12:17:26 +0100

    contact page kleuren van fb en twitter

--- project/resources/views/pages/contact.blade.php
+++ project/resources/views/pages/contact.blade.php
@@ -22,7 +22,7 @@
                         Business and Development <br>
                         <a href="mailto:rami@vlambeer.com" class="link">rami@vlambeer.com</a>
                          <br>
-                        <a href="https://twitter.com/search?q=tha_rami&src=typd" class="link">@tha_rami <i class="fa fa-twitter"></i></a> <br>
+                        <a href="https://twitter.com/search?q=tha_rami&src=typd" class="link">@tha_rami <i class="fa fa-twitter" style="color: #4099FF;"></i></a> <br>
                     </p>
                 </div>
                 <div class="col-xs-2 jan picture">
@@ -34,13 +34,13 @@
                         Game Design <br>
                         <a href="mailto:jw@vlambeer.com" class="link">jw@vlambeer.com</a>
                          <br>
-                        <a href="https://twitter.com/jwaaaap" class="link">@jwaaaap <i class="fa fa-twitter"></i></a> <br>
+                        <a href="https://twitter.com/jwaaaap" class="link">@jwaaaap <i class="fa fa-twitter" style="color: #4099FF;"></i></a> <br>
                     </p>
                 </div>
             </div>
             <div class="companyLocation col-xs-12">
                 <p>
-                    <a href="https://www.facebook.com/Vlambeer/?fref=ts" class="link">Like us on Facebook! <i class="fa fa-facebook-square"></i></a><br><br>
+                    <a href="https://www.facebook.com/Vlambeer/?fref=ts" class="link">Like us on Facebook! <i class="fa fa-facebook-square" style="color: #3b5998;"></i></a><br><br>
                     <a href="mailto:info@vlambeer.com" class="link">info@vlambeer.com</a> | Neude 5, 3512 AD<br>
                     Utrecht, The Netherlands | +316 21 20 63 63
                 </p>
