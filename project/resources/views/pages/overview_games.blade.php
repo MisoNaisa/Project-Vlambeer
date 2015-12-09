2ddc21fd61d7c35649a81c0612d66f4c69bb250c
dimitribeer <dimitrivandervliet@icloud.com>
2015-12-09 11:48:20 +0100

    new fix home and info
    JEWEETZELLUF

--- project/resources/views/pages/overview_games.blade.php
+++ project/resources/views/pages/overview_games.blade.php
@@ -8,7 +8,7 @@
         <div class="container">
             <div class="game-overview">
                 @foreach($gamesArray as $game)
-                    <div class="game-box col-md-5 col-md-push-1" onclick="javascript:location.href='info_game/{{$game['id']}}'">
+                    <div class="game-box col-md-5 col-md-push-1 clickable" onclick="javascript:location.href='info_game/{{$game['id']}}'">
                         <div class="img" style="background: url('{{$game['game_bg']}}')">
                             <div class="description">
                                 <h4> {!! $game['deck'] !!}</h4>
