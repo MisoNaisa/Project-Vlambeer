1c0600b9276e4ab48bcf5333f02fdba823c0f762
dimitribeer <dimitrivandervliet@icloud.com>
2015-12-16 10:01:36 +0100

    products table fix

--- project/database/seeds/DatabaseSeeder.php
+++ project/database/seeds/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
          $this->call(GamesTableSeeder::class);
+         $this->call(ProductsTableSeeder::class);
 
         Model::reguard();
     }
