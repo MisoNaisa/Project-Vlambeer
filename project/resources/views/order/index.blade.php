e046622b041e4929318bc1970ee94eeef764efcb
Teunimli <teunaarts@live.nl>
2016-01-07 09:09:36 +0100

    Overzicht orders admin gemaakt

--- project/resources/views/order/index.blade.php
+++ project/resources/views/order/index.blade.php
@@ -2,6 +2,21 @@
 
 @section('section')
     <div class="container">
+        <table>
+            <tr>
+                <td>First Name</td>
+                <td>Insertion</td>
+                <td>Last Name</td>
+                <td>Address</td>
+                <td>Housenumber</td>
+                <td>Zipcode</td>
+                <td>City</td>
+                <td>Country</td>
+                <td>Phonenumber</td>
+                <td>Order Date</td>
+                <td>Status</td>
+            </tr>
+        </table>
     @foreach($orders as $order)
         <table>
             <tr>
@@ -15,7 +30,7 @@
                 <td>{{$order['country']}}</td>
                 <td>{{$order['phonenumber']}}</td>
                 <td>{{$order['order_date']}}</td>
-                <td>{{$order['status']}}</td>
+                <td>@if($order['status'] == 0)Besteld @elseif($order['status'] == 1)Betaald@elseif($order['status'] == 2)Vertraagd @elseif($order['status'] == 3)Verzonden @elseif($order['status'] == 4)Geleverd @endif</td>
                 <td><a href="{{ action('OrdersController@edit', ['id' => $order->order_id]) }}">Bekijken</a></td>
             </tr>
         </table>
