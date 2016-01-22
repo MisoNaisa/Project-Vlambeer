24a11327b86d8ad491d9defdd7ffb555382efef9
kimvg2 <kimvg2@hotmail.com>
2016-01-22 10:59:31 +0100

    database aangepast.
    geen idee of dit ermee te maken heeft

--- project/vendor/composer/autoload_real.php
+++ project/vendor/composer/autoload_real.php
@@ -41,19 +41,15 @@ class ComposerAutoloaderInit26659feeab47855d753398663d30189c
         $loader->register(true);
 
         $includeFiles = require __DIR__ . '/autoload_files.php';
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire26659feeab47855d753398663d30189c($fileIdentifier, $file);
+        foreach ($includeFiles as $file) {
+            composerRequire26659feeab47855d753398663d30189c($file);
         }
 
         return $loader;
     }
 }
 
-function composerRequire26659feeab47855d753398663d30189c($fileIdentifier, $file)
+function composerRequire26659feeab47855d753398663d30189c($file)
 {
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-    }
+    require $file;
 }
